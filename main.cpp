ffd75041595df6d0f6fcd073bad24791d6e47356
Hamozmoz <hamozmoz10@gmail.com>
2026-09-14 15:02:57 +0300

    Registered TetriminoManager Singelton And The GameMatrix ListModel Into Qml

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include "GameManager.h"
+#include "Tetriminomanager.h"
+
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     qmlRegisterSingletonInstance("Tetris",1,0,"GameManager",&GameManager::Instance());
+    qmlRegisterSingletonInstance("Tetris",1,0,"TetriminoManager",&TetriminoManager::Instance());
+    //I Manually Registered The Type Because It Was Not Getting Registered With The QML_ELEMENT Macro
+    qmlRegisterUncreatableType<GameMatrix>("Tetris",1,0,"GameMatrix","Qml Uncreatable Type Cannot Be Created In Qml");
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
