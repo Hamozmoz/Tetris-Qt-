640e30e23b96e60d270b7e8ab3b4b405eb92b7ff
Hamozmoz <hamozmoz10@gmail.com>
2026-09-14 22:56:12 +0300

    Removed Useless Comment

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
     qmlRegisterSingletonInstance("Tetris",1,0,"GameManager",&GameManager::Instance());
     qmlRegisterSingletonInstance("Tetris",1,0,"TetriminoManager",&TetriminoManager::Instance());
-    //I Manually Registered The Type Because It Was Not Getting Registered With The QML_ELEMENT Macro
     qmlRegisterUncreatableType<GameMatrix>("Tetris",1,0,"GameMatrix","Qml Uncreatable Type Cannot Be Created In Qml");
     QObject::connect(
         &engine,
