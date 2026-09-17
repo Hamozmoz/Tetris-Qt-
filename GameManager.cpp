0527fb6016a39ec33ab004e992a01ef6fbf4a780
Hamozmoz <hamozmoz10@gmail.com>
2026-09-17 19:26:47 +0300

    Created GamePaused Property

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,9 +1,51 @@
 #include "GameManager.h"
+#include "Tetriminomanager.h"
+
+void GameManager::changeGamePaused(){
+    GamePaused = !GamePaused;
+    GamePausedChanged();
+    if(GamePaused){
+    TetriminoManager::Instance().TetriminoTimer->stop();
+    TetriminoManager::Instance().FrameTimer->stop();
+    }else{
+        TetriminoManager::Instance().FrameTimer->start();
+    }
+
+}
 
 GameManager &GameManager::Instance() {
     static GameManager manager;
     return manager;
 }
 
+void GameManager::ChangeGameState(GameState State){
+    if(CurrentGameState != State){
+        CurrentGameState = State;
+        emit GameStateChanged();
+    }
+}
 
-GameManager::GameManager() {}
+const bool GameManager::readDebugMode() const{
+    return DebugMode;
+}
+
+const bool GameManager::readGamePaused() const {
+    return GamePaused;
+}
+
+GameManager::GameState GameManager::GetGameState(){
+    return CurrentGameState;
+}
+
+
+GameManager::GameManager() {
+#ifndef NDEBUG
+    DebugMode = true;
+#endif
+}
+
+
+
+
+
+
--- GameManager.h
+++ GameManager.h
@@ -6,27 +6,27 @@ class GameManager: public QObject
 {
     Q_OBJECT
     Q_PROPERTY(GameState CurrentGameState READ GetGameState NOTIFY GameStateChanged)
+    Q_PROPERTY(bool DebugMode READ readDebugMode CONSTANT)
+    Q_PROPERTY(bool GamePaused READ readGamePaused NOTIFY GamePausedChanged)
 public:
     enum GameState{
         OpeningScreen,
         GameRunning
     };
     Q_ENUM(GameState)
+    Q_INVOKABLE void changeGamePaused();
     GameState CurrentGameState = GameState::OpeningScreen;
    static GameManager& Instance ();
-    GameState GetGameState(){
-        return CurrentGameState;
-    }
-    void ChangeGameState(GameState State){
-        if(CurrentGameState != State){
-            CurrentGameState = State;
-            emit GameStateChanged();
-        }
-    }
+    GameState GetGameState();
+    void ChangeGameState(GameState State);
+    const bool readDebugMode() const;
+    const bool readGamePaused() const;
 signals :
 void GameStateChanged();
-
+    void GamePausedChanged();
 private:
+bool GamePaused{false};
+bool DebugMode {false};
     GameManager();
 };
 
