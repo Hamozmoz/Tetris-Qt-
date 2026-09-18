b8054fb741fa80007c9b88e502582d393151a3b0
Hamozmoz <hamozmoz10@gmail.com>
2026-09-18 14:17:19 +0300

    Created loseGame Function

--- Tetriminomanager.h
+++ Tetriminomanager.h
@@ -19,63 +19,65 @@ public:
     Right
     };
     enum LineState: uint8_t{
-        LineCleared,
-        LineUncleared,
-        LineEmpty
+    LineCleared,
+    LineUncleared,
+    LineEmpty
     };
 
-    Q_ENUM(Direction)
-    int Score{0};
-    bool GameOver{false};
-    bool DebugMode{false};
-    bool FastDrop{false};
-    std::array<Position,4> FinalDropPosition;
-    Tetrimino CurrentTetrimino ;
-    QChronoTimer* FrameTimer ;
-    QChronoTimer* TetriminoTimer ;
-    static TetriminoManager& Instance();
-    GameMatrix* getGameGrid();
-    int readScore();
-    const bool readGameOver()const;
-    const bool readDebugMode()const;
-   Q_INVOKABLE  void moveTetrimino(Direction dir);
-   Q_INVOKABLE   void addTetriminoToGameGrid();
-   Q_INVOKABLE void moveDown();
-   Q_INVOKABLE void startGame();
-   Q_INVOKABLE void changeFastDropToTrue();
-   Q_INVOKABLE void changeFastDropToFalse();
-   Q_INVOKABLE void rotateTetrimino();
-   Q_INVOKABLE void changeDebugMode();
-   Q_INVOKABLE void checkLines();
-   Q_INVOKABLE void changeTileColor(int index);
-   Q_INVOKABLE void exitGame();
-   Q_INVOKABLE void instantDrop();
-   signals :
-   void ScoreChanged();
-   void DebugModeChanged();
-   void GameOverChanged();
+Q_ENUM(Direction)
+int Score{0};
+bool GameOver{false};
+bool DebugMode{false};
+bool FastDrop{false};
+std::array<Position,4> FinalDropPosition;
+Tetrimino CurrentTetrimino ;
+QChronoTimer* FrameTimer ;
+QChronoTimer* TetriminoTimer ;
+static TetriminoManager& Instance();
+GameMatrix* getGameGrid();
+int readScore();
+const bool readGameOver()const;
+const bool readDebugMode()const;
+Q_INVOKABLE  void moveTetrimino(Direction dir);
+Q_INVOKABLE   void addTetriminoToGameGrid();
+Q_INVOKABLE void moveDown();
+Q_INVOKABLE void startGame();
+Q_INVOKABLE void changeFastDropToTrue();
+Q_INVOKABLE void changeFastDropToFalse();
+Q_INVOKABLE void rotateTetrimino();
+Q_INVOKABLE void changeDebugMode();
+Q_INVOKABLE void checkLines();
+Q_INVOKABLE void changeTileColor(int index);
+Q_INVOKABLE void exitGame();
+Q_INVOKABLE void instantDrop();
+
+ signals :
+void ScoreChanged();
+void DebugModeChanged();
+void GameOverChanged();
  private:
-   void ClearLine();
-     void WallKick(Tetrimino &TetriminoToTest);
+void ClearLine();
+void WallKick(Tetrimino &TetriminoToTest);
 Rotation NextRotation();
-     void reset();
+void reset();
+void loseGame();
 void CalculateFinalDropPositon();
-     const bool CheckCanMoveDown(const std::array<Position,4> Positions);
-     const bool CheckCanMoveDown();
-     bool CanMoveDown{false};
-     void InitTetriminoBag();
-     bool CanRotate(const Tetrimino& tetrimino);
-    void GameLoop();
-     void TurnCurPosOpaque();
-     TetriminoManager();
- std::random_device SeedGen;
-    uint32_t Seed = SeedGen();
- inline void RestartTetriminoTimer();
- void rotateTetrimino(Rotation GoalRotation);
+const bool CheckCanMoveDown(const std::array<Position,4> Positions);
+const bool CheckCanMoveDown();
+bool CanMoveDown{false};
+void InitTetriminoBag();
+bool CanRotate(const Tetrimino& tetrimino);
+void GameLoop();
+void TurnCurPosOpaque();
+TetriminoManager();
+std::random_device SeedGen;
+uint32_t Seed = SeedGen();
+inline void RestartTetriminoTimer();
+void rotateTetrimino(Rotation GoalRotation);
 void SetTetrimino();
 std::array<TetriminoType,7> TetriminoBag;
 std::array<LineState,Rows-2> LineStates;
-    GameMatrix GameGrid;
+GameMatrix GameGrid;
 };
 Position max(Position pos1,Position pos2);
 Position min(Position pos1,Position pos2);
