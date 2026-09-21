bf450b85eb8b95458a5151f20dec973dfde6397a
Hamozmoz <hamozmoz10@gmail.com>
2026-09-21 21:11:15 +0300

    Changed Rows And Columns Into Constexpr Values

--- GameMatrix.h
+++ GameMatrix.h
@@ -3,10 +3,8 @@
 #include <QAbstractListModel>
 #include <qqmlintegration.h>
 #include <qtmetamacros.h>
-#define Rows 22
-#define Columns 10
-
-
+constexpr int Rows = 22;
+constexpr int Columns = 10;
 struct Position{
     int8_t column;
     int8_t row;
